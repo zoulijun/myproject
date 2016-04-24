76de3ef57ffb63888665ad1daec3b28336c72ad4
unknown <>
2016-04-24 22:52:57 +0800

    '电脑管理系统1'

---
+++ src/Computersms.java
@@ -0,0 +1,47 @@
+package com.mycode.zpc;
+public class Computersms
+{	Computer arr[] = new Computer[6];
+	public void Sava(Computer computer){
+	//储存电脑信息
+	
+	}
+	public void SavaColour(String name){
+	//储存每种电脑的颜色
+	
+	}
+	public int QueryPrice(String name){
+		return 0;
+	//查询每种电脑的价格
+	}
+	public String QueryModel(String name){
+		return null;
+	//查询每种电脑的型号
+	
+	}
+	public String QueryColour(String name){
+		return null;
+	//查询每种电脑的型号
+	}
+	public Computer QueryComputer(Computer computer){
+		return null;
+	//查询所有储存的电脑
+	}
+	public void DeleteComputer(String name){
+	
+	//删除电脑信息
+	
+	}
+	public void AlterComputer(String name){
+	
+	//修改电脑信息
+	
+	}
+public static void main(String[] args){
+	System.out.println("***************电脑管理系统******************");
+
+
+
+
+}
+
+}
